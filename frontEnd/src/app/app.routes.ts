dfba53c534c1e14183acca149fe82776832381fb
Chiwii8 <alejandrosanrod@outlook.com>
2025-02-26 21:01:36 +0100

    update: add new router and routerlink to login

--- frontEnd/src/app/app.routes.ts
+++ frontEnd/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserListComponent } from './core/user/component/user-list/user-list.component';
 import { UserFormComponent } from './core/user/component/user-form/user-form.component';
 import { NgModule } from '@angular/core';
+import { LoginFormComponent } from './core/auth/component/login-form/login-form.component';
 
 export const routes: Routes = [
     {path: 'users', component: UserListComponent},
     {path: 'adduser', component: UserFormComponent},
+    {path:'login', component: LoginFormComponent},
     {path: '', redirectTo:'/users', pathMatch:'full'}
 ];
 
